1330159ad6d1c62b5d38aa9ffcb82d6f5087c618
Lucas Nguyen <lucasoAlt@outlook.com>
2025-01-01 15:54:06 +0100

    feat: automatically change active link in BottomDashNavbar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,25 +1,31 @@
+"use client";
+
+import { usePathname} from "next/navigation";
+import { useEffect, useState } from "react";
+
 import Navbar from "@/components/DashNavbar";
 import Footer from "@/components/Footer";
 import BottomDashNavbar from "@/components/BottomDashNavbar";
 
-export default async function Layout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    
-    const links = [
-        { name: "Sites", href: "/dashboard" },
-        { name: "Account settings", href: "/dashboard/account-setting" },
-    ];
+const links = [
+    { name: "Sites", href: "/dashboard" },
+    { name: "Account settings", href: "/dashboard/account" },
+];
 
-    const currentPage = "/dashboard";
+export default function Layout({ children }: { children: React.ReactNode }) {
+    const pathname = usePathname();
+    const [currentPage, setCurrentPage] = useState(pathname);
+
+    useEffect(() => {
+        setCurrentPage(pathname);
+    }, [pathname]);
+
     return (
         <main className="font-sans">
-            <Navbar></Navbar>
+            <Navbar />
             <BottomDashNavbar links={links} currentPage={currentPage} />
             {children}
-            <Footer></Footer>
+            <Footer />
         </main>
     );
 }
